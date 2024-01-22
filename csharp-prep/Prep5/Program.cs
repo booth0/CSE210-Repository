9b0e10799eef4106889de13d93ec40d83288fa1e
booth0 <10toasterv2@gmail.com>
2024-01-22 09:36:45 -0700

    new

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -40,4 +40,5 @@ class Program
         }
         // y = w + 4;
     }
+
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
+class Car {
+public int milesPerGallon;
+public int gallons;
+public string make;
+public string model;
+
+    
+}
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        var cars = new List<Car>();
+
+        var car = new Car();
+        car.make = "Honda";
+        car.model = "Civic";
+        car.gallons = 10;
+        car.milesPerGallon = 30;
+        cars.Add(car);
+
+        car = new Car();
+        car.make = "Toyota";
+        car.model = "Corolla";
+        car.gallons = 12;
+        car.milesPerGallon = 35;
+        cars.Add(car);
+
+        foreach (var c in cars) {
+            System.Console.WriteLine($"{c.make} {c.model}: Range={c.gallons * c.milesPerGallon}");
+        }
     }
 }
