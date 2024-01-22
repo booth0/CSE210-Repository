5521f9331d8d6b7acff1e1a09971ae92e43ccc4a
booth0 <10toasterv2@gmail.com>
2024-01-22 09:05:26 -0700

    today

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
-        List<int> numbers = new List<int>();
+        List<float> numbers = new List<float>();
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         bool isZero = false;
         while (isZero == false){
             Console.WriteLine("Enter number: ");
-            var num = int.Parse(Console.ReadLine());
+            var num = float.Parse(Console.ReadLine());
             if (num == 0){
                 isZero = true;
             }
@@ -20,9 +21,23 @@ class Program
                 numbers.Add(num);
             }
         }
+        float sum = 0;
+        float largestNum = -99999;
+        foreach (float number in numbers)
+        {
+            sum += number;
+            Console.WriteLine(number);
+            if(number > largestNum){
+                largestNum = number;
+            }
+            else{}
 
-        Console.WriteLine(numbers);
-        
+        }
+
+        float avg = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {largestNum}");
         
         
     }
